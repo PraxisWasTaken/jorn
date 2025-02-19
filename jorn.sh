5559fd656fcf83a755f4e99401ff92f5c4952176
Praxis <131200250+PraxisWasTaken@users.noreply.github.com>
2025-02-18 18:58:16 -0800

    Update jorn.sh
    existing file verification added

--- jorn.sh
+++ jorn.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+simpleDate=$(date +"%Y-%m-%d") # example output: 2025-01-24
 
-simpleDate=$(date +"%Y-%m-%d") #example output: 2025-01-24
-
-complexDate=$(date +"%B %d, %Y") #example output: January 24, 2025
+complexDate=$(date +"%B %d, %Y") # example output: January 24, 2025
 
 pathToDoc="/path/to/desired/directory/"
 
+file="${pathToDoc}${simpleDate}.jrnl"
+
+# Check if the file already exists
+if [[ -e "$file" ]]; then
+    echo "Error: The file '$file' already exists. Aborting."
+    exit 1
+fi
+
+# Create the file
 echo -e "Date: ${complexDate}\n\
 Tags:\n\
 \n\
 Journal:\n\
 \n\
 \n\
-To-Do:\n" > "${pathToDoc}${simpleDate}.jrnl"
+To-Do:\n" > "$file"
+
+echo "Journal entry created: $file"
